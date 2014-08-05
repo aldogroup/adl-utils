3157e945b7b2693afc5af7bb1a73c2bee95d267a
Badi Labassi <blabassi@aldogroup.com>
2014-08-05 07:38:22 -0400

    Splitted again the big method

--- lib/adl-utils/commands/impex/confirm.rb
+++ lib/adl-utils/commands/impex/confirm.rb
@@ -11,6 +11,17 @@ module Middleman
 
       no_commands do
 
+        def confirm_generate(config={}, locale, impex_file)
+          build_dir = Pathname.new("build/#{config[:revision]}/hybris/" + locale)
+          confirm_file = impex_file
+
+          # =>  Read page and get content
+          config[:impex_data].to_a.each do |page|
+            next if page['page_title'] == 'homepage'
+            confirm_page_generator(build_dir, page, confirm_file)
+          end # End of confirm_impex_pages loop
+        end # End of the generate method
+
         def landing_page_impex(file_destination, page={}, content)
           append_to_file file_destination, :verbose => false do
             "##{page['page_title']}\n;;#{page['hybris_id']};\"#{content}\";\"\";\n"
@@ -23,38 +34,25 @@ module Middleman
           end
         end
 
-        def confirm_generate(config={}, locale, impex_file)
+        def confirm_page_generator(build_dir, page={}, confirm_file)
+          content = File.join(build_dir, page['page_file'])
+          content_page = File.read(content).gsub(' "', '"').gsub('"', '""').force_encoding('ASCII-8BIT')
 
-          build_dir = Pathname.new("build/#{config[:revision]}/hybris/" + locale)
-          confirm_file = impex_file
+          # Generate the rest of the content
+          append_to_file confirm_file, :verbose => false do
+            "\n# Landing Pages & Category Banner\n$productCatalog=#{config[:country_code]}AldoProductCatalog\n$catalogVersion=catalogversion(catalog(id[default=$productCatalog]),version[default='Staged'])[unique=true,default=$productCatalog:Staged]\nUPDATE Category;$catalogVersion;code[unique=true];landingPage[lang=$lang];categoryBanner[lang=$lang]\n"
+          end
 
-          # =>  Read page and get content
-          config[:impex_data].to_a.each do |page|
+          if page['type'] == 'landing page'
+            landing_page_impex(confirm_file, page, content_page)
+          else
+            category_banner_impex(confirm_file, page, content_page)
+          end
 
-            content = File.join(build_dir, page['page_file'])
-            content_page = File.read(content).gsub(' "', '"').gsub('"', '""').force_encoding('ASCII-8BIT')
-
-            next if page['page_title'] == 'homepage'
-            binding.pry
-            # Generate the rest of the content
-
-            append_to_file confirm_file, :verbose => false do
-              "\n# Landing Pages & Category Banner\n$productCatalog=#{config[:country_code]}AldoProductCatalog\n$catalogVersion=catalogversion(catalog(id[default=$productCatalog]),version[default='Staged'])[unique=true,default=$productCatalog:Staged]\nUPDATE Category;$catalogVersion;code[unique=true];landingPage[lang=$lang];categoryBanner[lang=$lang]\n"
-            end
-
-            if page['type'] == 'landing page'
-              landing_page_impex(confirm_file, page, content_page)
-            else
-              category_banner_impex(confirm_file, page, content_page)
-            end
-
-            if page.include?('sub_pages')
-              sub_pages(build_dir, page)
-            end # End of sub_pages conditional check
-
-          end # End of confirm_impex_pages loop
-
-        end # End of the generate method
+          if page.include?('sub_pages')
+            sub_pages(build_dir, page)
+          end # End of sub_pages conditional check
+        end
 
         def sub_pages(build_dir, page)
           page['sub_pages'].each do |sub_page|
@@ -71,7 +69,7 @@ module Middleman
               append_to_file(confirm_file, "##{page['page_title']} #{sub_page['page_title']}\n;;#{sub_page['hybris_id']};\"#{sub_content_page}\"\n", :verbose => false)
             end # End of check for page type inside sub_pages
           end # End of sub_pages generator loop
-          
+
         end
       end
     end
