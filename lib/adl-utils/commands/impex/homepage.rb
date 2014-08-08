3a4a782f7ef9cf581359b72c41152c2e2ebbea0f
Badi Labassi <blabassi@aldogroup.com>
2014-08-08 18:58:34 -0400

    Splitted what I could in methods

--- lib/adl-utils/commands/impex/homepage.rb
+++ lib/adl-utils/commands/impex/homepage.rb
@@ -34,6 +34,42 @@ module Middleman
           (content.upcase.gsub(/[^a-zA-Z 0-9]/, '')).gsub(/\s/,'-')
         end
 
+        def gentime
+          Time.now.strftime('%y-%m-%d_%H.%M')
+        end
+
+        def output_dir
+          "build/impex/#{ENV['REV']}"
+        end
+
+        def template_source
+          File.join( @template_dir + 'impex_content.erb')
+        end
+
+        def homepage_filepath(dir)
+          File.join(dir, 'index.html')
+        end
+
+        def head_content(dir)
+          impexify_content(File.read(File.join(dir, '/head.html')))
+        end
+
+        def footer_content(dir)
+          impexify_content(File.read(File.join(dir, '/footer.html')))
+        end
+
+        def content_page
+          impexify_content(File.read(content))
+        end
+
+        def fr_swap(content)
+          content.gsub('ca_en', 'ca_fr')
+        end
+
+        def page_fr(content_fr)
+          impexify_content(File.read(content_fr))
+        end
+
         def content_var(mm_config={}, locale)
 
           impex_property = Hash.new
@@ -67,8 +103,8 @@ module Middleman
         def generate_config(mm_config={})
           Middleman::Cli::GenerateHomepage.source_root('../')
           @template_dir = File.join(File.dirname(__FILE__), '/data/')
-          campaign =  mm_config[:campaign]
-          output_file = "#{output_dir}/#{gentime}_#{campaign}_config.impex"
+          @campaign =  mm_config[:campaign]
+          output_file = "#{output_dir}/#{gentime}_#{@campaign}_config.impex"
           say("\n══ Generating impex config file", :green)
           copy_file(File.join( @template_dir + 'impex_config.tt'), output_file)
           mm_config[:hybris_locales].each do |loc|
@@ -77,45 +113,19 @@ module Middleman
           end
         end
 
-        def gentime
-          Time.now.strftime('%y-%m-%d_%H.%M')
-        end
-
-        def output_dir
-          "build/impex/#{ENV['REV']}"
-        end
-
         def generate_homepage(mm_config={}, locale)
           build_dir = File.join(mm_config[:build_dir].to_s, locale.to_s)
-          campaign =  mm_config[:campaign]
           country_code = mm_config[:country_code]
-          impex_homepage_file = "#{output_dir}/#{gentime}_#{campaign}-homepage_#{country_code}.impex"
-          template_source = File.join( @template_dir + 'impex_content.erb')
+          impex_homepage_file = "#{output_dir}/#{gentime}_#{@campaign}-homepage_#{country_code}.impex"
           opts = Hash.new
-
-          opts[:homepage_content] = File.read(File.join(build_dir, 'index.html'))
-          opts[:homepage_content_fr] = page_fr(fr_swap(File.join(build_dir, 'index.html'))) if locale.to_s =='ca_en'
-          opts[:head_content] = impexify_content(File.read(File.join(build_dir, '/head.html')))
-          opts[:footer_content] = impexify_content(File.read(File.join(build_dir, '/footer.html')))
-
+          opts[:head_content] = head_content(build_dir)
+          opts[:homepage_content] = impexify_content(File.read(homepage_filepath(build_dir)))
+          opts[:homepage_content_fr] = page_fr(fr_swap(homepage_filepath(build_dir))) if locale.to_s =='ca_en'
+          opts[:footer_content] = footer_content(build_dir)
           template(template_source, impex_homepage_file, mm_config.merge(opts))
-
-        end
-
-        def content_page
-          impexify_content(File.read(content))
-        end
-
-        def fr_swap(content)
-          content.gsub('ca_en', 'ca_fr')
-        end
-
-        def page_fr(content_fr)
-          impexify_content(File.read(content_fr))
         end
 
       end
-
     end
   end
 end
