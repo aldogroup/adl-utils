dffd8bee1d708e1d8a6f5071166fd7bf57d95703
Badi Labassi <blabassi@aldogroup.com>
2015-05-12 17:08:18 -0400

    Bumped

--- lib/adl-utils/commands/impex/confirm.rb
+++ lib/adl-utils/commands/impex/confirm.rb
@@ -15,7 +15,7 @@ module Middleman
       no_commands do
 
         def product_catalog
-          if @config[:brand] == 'Aldo-Shoes'
+          if @config[:brand] == 'Aldo-Shoes' || @config[:brand] == 'ALDO-SHOES'
             return @config[:country_code]+ 'AldoProductCatalog'
           elsif @config[:brand] == 'Call-it-Spring'
             return @config[:country_code]+ 'CISProductCatalog'
